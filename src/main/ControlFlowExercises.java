58678138bac16ec9bf12db8ece6de5285ba5f187
withers56 <william.k.withers@gmail.com>
2022-03-01 13:51:49 -0600

    do-while 3rd exercise

--- src/main/ControlFlowExercises.java
+++ src/main/ControlFlowExercises.java
@@ -2,20 +2,40 @@ package main;
 
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        int i = 5;
+//        int i = 5;
 
-        while (i <= 15){
+//        while (i <= 15){
+//            System.out.println(i);
+//            i++;
+//        }
+
+        for (int i = 5; i <= 15; i++) {
             System.out.println(i);
-            i++;
         }
 
-        int j = 100;
+//        int j = 100;
+//
+//        do {
+//            System.out.println(j);
+//
+//            j -= 5;
+//
+//        }while (j >= -10);
 
-        do {
-            System.out.println(j);
+        for (int i = 100; i >= -10; i -= 5) {
+            System.out.println(i);
+        }
 
-            j -= 5;
+//        long k = 2;
+//
+//        do {
+//            System.out.println(k);
+//
+//            k = k * k;
+//        }while (k <= 1000000);
 
-        }while (j >= -10);
+        for (long i = 2; i <= 1000000 ; i = i * i) {
+            System.out.println(i);
+        }
     }
 }
