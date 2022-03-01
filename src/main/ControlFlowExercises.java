8fcfd44ad44ef88e3f6460187459ad29adb1e147
withers56 <william.k.withers@gmail.com>
2022-03-01 13:58:12 -0600

    fizzbuzz exercise

--- src/main/ControlFlowExercises.java
+++ src/main/ControlFlowExercises.java
@@ -37,5 +37,20 @@ public class ControlFlowExercises {
         for (long i = 2; i <= 1000000 ; i = i * i) {
             System.out.println(i);
         }
+
+
+        for (int i = 1; i <= 100; i++) {
+
+           if (i % 3 == 0 && i % 5 == 0){
+               System.out.println(i + ": FizzBuzz");
+           }
+           else if (i % 3 == 0){
+                System.out.println(i + ": fizz");
+            }
+            else if (i % 5 == 0){
+                System.out.println(i + ": buzz");
+            }
+            else System.out.println(i);
+        }
     }
 }
