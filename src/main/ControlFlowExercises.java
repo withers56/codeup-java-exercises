3aa4923472a66bf9ba07a903f4c15002f3d6cdab
withers56 <william.k.withers@gmail.com>
2022-03-01 14:14:05 -0600

    table exercise

--- src/main/ControlFlowExercises.java
+++ src/main/ControlFlowExercises.java
@@ -1,5 +1,7 @@
 package main;
 
+import java.util.Scanner;
+
 public class ControlFlowExercises {
     public static void main(String[] args) {
 //        int i = 5;
@@ -9,9 +11,9 @@ public class ControlFlowExercises {
 //            i++;
 //        }
 
-        for (int i = 5; i <= 15; i++) {
-            System.out.println(i);
-        }
+//        for (int i = 5; i <= 15; i++) {
+//            System.out.println(i);
+//        }
 
 //        int j = 100;
 //
@@ -22,9 +24,9 @@ public class ControlFlowExercises {
 //
 //        }while (j >= -10);
 
-        for (int i = 100; i >= -10; i -= 5) {
-            System.out.println(i);
-        }
+//        for (int i = 100; i >= -10; i -= 5) {
+//            System.out.println(i);
+//        }
 
 //        long k = 2;
 //
@@ -34,23 +36,43 @@ public class ControlFlowExercises {
 //            k = k * k;
 //        }while (k <= 1000000);
 
-        for (long i = 2; i <= 1000000 ; i = i * i) {
-            System.out.println(i);
+//        for (long i = 2; i <= 1000000 ; i = i * i) {
+//            System.out.println(i);
+//        }
+
+
+//        for (int i = 1; i <= 100; i++) {
+//
+//           if (i % 3 == 0 && i % 5 == 0){
+//               System.out.println(i + ": FizzBuzz");
+//           }
+//           else if (i % 3 == 0){
+//                System.out.println(i + ": fizz");
+//            }
+//            else if (i % 5 == 0){
+//                System.out.println(i + ": buzz");
+//            }
+//            else System.out.println(i);
+//        }
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("What number would you like to go to?: ");
+        int userNumber = scanner.nextInt();
+        System.out.println("    Here is your table!");
+        System.out.println("|--------|----------|----------|");
+        System.out.println("| Number | Squared  | Cubed    |");
+        System.out.println("|--------|----------|----------|");
+
+
+        for (int i = 1; i <= userNumber; i++) {
+
+            int iSquared = i * i;
+            int iCubed = i * i * i;
+
+            System.out.printf("| %-7d|   %-7d|   %-7d|\n", i, iSquared, iCubed);
         }
+        System.out.println("|--------|----------|----------|");
 
-
-        for (int i = 1; i <= 100; i++) {
-
-           if (i % 3 == 0 && i % 5 == 0){
-               System.out.println(i + ": FizzBuzz");
-           }
-           else if (i % 3 == 0){
-                System.out.println(i + ": fizz");
-            }
-            else if (i % 5 == 0){
-                System.out.println(i + ": buzz");
-            }
-            else System.out.println(i);
-        }
     }
 }
