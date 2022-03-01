e36f583c0ef8967fbce907ecaa3e1c781a73addd
withers56 <william.k.withers@gmail.com>
2022-03-01 13:44:46 -0600

    do-while exercise

--- src/main/ControlFlowExercises.java
+++ src/main/ControlFlowExercises.java
@@ -2,6 +2,20 @@ package main;
 
 public class ControlFlowExercises {
     public static void main(String[] args) {
+        int i = 5;
 
+        while (i <= 15){
+            System.out.println(i);
+            i++;
+        }
+
+        int j = 0;
+
+        do {
+            if (j % 2 == 0){
+                System.out.println(j);
+            }
+            j++;
+        }while (j <= 100);
     }
 }
