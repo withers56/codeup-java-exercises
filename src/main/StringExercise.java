db92930a936d0de686358db822f8b9ba02e049c2
withers56 <william.k.withers@gmail.com>
2022-03-02 11:08:07 -0600

    finished stringexercise

--- src/main/StringExercise.java
+++ src/main/StringExercise.java
@@ -3,5 +3,23 @@ package main;
 public class StringExercise {
     public static void main(String[] args) {
 
+        String phrase = "We dont need no";
+
+        System.out.println(phrase + " education");
+        System.out.println(phrase + " thought control");
+
+        String secondPhrase = "Check \"this\" out, \"s inside of \"s!";
+
+        System.out.println(secondPhrase);
+
+        String thirdPhrase = "In windows, the main drive is usually C:\\";
+
+        System.out.println(thirdPhrase);
+
+        String fourthPhrase = "I can do backslashes \\, double backslashes \\\\,\n" +
+                "and the amazing triple backslash \\\\\\!";
+
+        System.out.println(fourthPhrase);
+
     }
 }
