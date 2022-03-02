9f6f932ae3e25514b65a9c8341f7333aaa89ce10
withers56 <william.k.withers@gmail.com>
2022-03-02 11:11:09 -0600

    finished stringexercise

--- src/main/Bob.java
+++ src/main/Bob.java
@@ -1,7 +1,17 @@
 package main;
 
+import java.util.Scanner;
+
 public class Bob {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ask Bob something!: ");
+        String userInput = scanner.nextLine();
+
+        System.out.println(userInput);
+
 
     }
 }
