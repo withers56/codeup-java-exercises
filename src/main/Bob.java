463fa9682d5c3873764105cc24e0807a47995d1d
withers56 <william.k.withers@gmail.com>
2022-03-02 11:22:31 -0600

    Finished bob exercise

--- src/main/Bob.java
+++ src/main/Bob.java
@@ -7,11 +7,36 @@ public class Bob {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Ask Bob something!: ");
-        String userInput = scanner.nextLine();
+        boolean willContinue = true;
 
-        System.out.println(userInput);
+        while (willContinue){
+
+            System.out.print("Ask Bob something!: ");
+            String userInput = scanner.nextLine();
+
+            System.out.println("You: " + userInput);
+
+            if (userInput.endsWith("?")){
+                System.out.println("Bob: Sure.");
+            }
+            else if (userInput.endsWith("!")){
+                System.out.println("Bob: Whoa, chill out!");
+            }
+            else if (userInput.length() == 0){
+                System.out.println("Bob: Fine, be that way!");
+            }
+            else System.out.println("Bob: Whatever.");
 
 
+            System.out.println("Ask something else? [y/n]");
+            String confirmation = scanner.next();
+
+            if (confirmation.equals("n")){
+                willContinue = false;
+            }
+        }
+        System.out.print("Bob: Catch you later nerd!");
     }
 }
+
+
