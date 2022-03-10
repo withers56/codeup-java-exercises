b7d77c4508a6287f27918cdd1b513561f6e32998
withers56 <william.k.withers@gmail.com>
2022-03-10 11:36:38 -0600

    started arryas exercises, completed 1 and 2

--- src/main/ServerNameGenerator.java
+++ src/main/ServerNameGenerator.java
@@ -5,5 +5,14 @@ public class ServerNameGenerator {
     String [] adjArray = {"snobbish", "grotesque", "necessary", "abounding", "severe", "profuse", "nutritious", "wide-eyed", "thankful", "heavy"};
     String[] nounArray = {"atmosphere", "performance", "supermarket", "difference", "response", "childhood", "instruction", "phone", "direction", "percentage"};
 
+    public String getRandomName() {
+        return adjArray[(int) Math.floor(Math.random() * 10)] + "-" + nounArray[(int) Math.floor(Math.random() * 10)];
+    }
 
+    public static void main(String[] args) {
+        ServerNameGenerator generator = new ServerNameGenerator();
+
+        System.out.print("Here is your server name: ");
+        System.out.println(generator.getRandomName());
+    }
 }
