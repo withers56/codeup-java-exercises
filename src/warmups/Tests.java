fb1655109d918f60f65ba0965f264e1d9f6589c8
withers56 <william.k.withers@gmail.com>
2022-03-24 10:31:13 -0500

    quiz tests

---
+++ src/warmups/Tests.java
@@ -0,0 +1,16 @@
+package warmups;
+
+import java.util.ArrayList;
+import java.util.List;
+public class Tests {
+    public static void main(String[] args) {
+       ArrayList<Integer> num = new ArrayList<>();
+
+    }
+
+    public static Exception getDepartment() {
+
+            throw new IllegalArgumentException("No department found for ");
+
+    }
+}
