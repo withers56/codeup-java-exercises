b2e95836e07380dea4543a82b28d660b62b4ae3e
withers56 <william.k.withers@gmail.com>
2022-06-14 11:16:06 -0500

    anagram problem

--- src/main/Tests.java
+++ src/main/Tests.java
@@ -7,24 +7,111 @@ import java.util.List;
 
 public class Tests {
 
-    public static final String ANSI_GREEN = "\033[1;32m";
-    public static final String ANSI_BRIGHT_GREEN = "\033[1;92m";
-    public static final String ANSI_BLACK_BACKGROUND = "\u001B[40m";
-    public static final String ANSI_RESET = "\u001B[0m";
-
-
     public static void main(String[] args) {
 
-        String name = "will";
-        int num = 9;
-        System.out.print(ANSI_BLACK_BACKGROUND);
-        System.out.print(ANSI_GREEN + "test");
-        System.out.print(ANSI_RESET);
-        System.out.print(ANSI_BLACK_BACKGROUND);
-        System.out.print(ANSI_BRIGHT_GREEN + "<(-_-)>");
+    int [] candies = {6,6,6,6};
+
+//    maxProfit(prices);
+//        System.out.println(distributeCandies(candies));
+
+//        System.out.println(fib(4));
+        System.out.println(isAnagram("anagram", "nagaram"));
+
+
+    }
+    public static boolean isAnagram(String s, String t) {
+
+        int firstStringVal = 0;
+        int secondStringVal = 0;
 
 
 
+        String[] firstStringChars = s.split("");
+        String[] secondStringChars = t.split("");
 
+        ArrayList<String> firstCharsArrayList = new ArrayList<>();
+
+        if (firstStringChars.length != secondStringChars.length) {
+            return false;
+        }
+
+        for (int i = 0; i < firstStringChars.length; i++) {
+            firstCharsArrayList.add(firstStringChars[i]);
+            firstStringVal += firstStringChars[i].codePointAt(0);
+        }
+
+        for (int i = 0; i < secondStringChars.length; i++) {
+
+            if (!firstCharsArrayList.contains(secondStringChars[i])) {
+                return false;
+            }
+
+            secondStringVal += secondStringChars[i].codePointAt(0);
+        }
+
+        System.out.println(firstStringVal);
+        System.out.println(secondStringVal);
+
+        if (firstStringVal == secondStringVal) {
+            return true;
+        }
+        return false;
+    }
+
+    public static int distributeCandies(int[] candyType) {
+        ArrayList<Integer> uniCandies = new ArrayList<>();
+
+        for (int i = 0; i < candyType.length; i++) {
+            int currentCandy = candyType[i];
+            if (!uniCandies.contains(currentCandy)) {
+                uniCandies.add(currentCandy);
+            }
+        }
+
+        if (candyType.length / 2 < uniCandies.size()) {
+            return candyType.length / 2;
+        }
+
+        return uniCandies.size();
+    }
+
+
+    public static int fib(int n) {
+        if (n == 0) {
+            return 0;
+        }
+
+        if (n == 1) {
+            return 1;
+        }
+
+        return fib(n - 1) + fib(n - 2);
+    }
+
+    public static int maxProfit(int [] prices) {
+        int profit = 0;
+
+        for (int i = 0; i < prices.length; i++) {
+            int priceOfDay = prices[i];
+            int priceOfPreviousDay = 0;
+
+            if (i != 0) {
+                priceOfPreviousDay = prices[i - 1];
+            }
+            int dailyProfit = getDailyProfit(priceOfPreviousDay, priceOfDay);
+
+
+            if (priceOfDay > priceOfPreviousDay) {
+
+            }
+
+
+        }
+
+        return 1;
+    }
+
+    public static int getDailyProfit(int buyPrice, int sellPrice) {
+        return sellPrice - buyPrice;
     }
 }
