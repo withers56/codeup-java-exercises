c38b644bc25a30f24449625d88857642a1f4f12f
withers56 <william.k.withers@gmail.com>
2022-06-22 11:15:37 -0500

    completed valid palindrome exercise

--- src/leetcodes/ValidPalindrome.java
+++ src/leetcodes/ValidPalindrome.java
@@ -6,26 +6,35 @@ import java.util.Arrays;
 public class ValidPalindrome {
 //    A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all
 //    non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+//    Runtime: 10 ms, faster than 46.61% of Java online submissions for Valid Palindrome.
+//    Memory Usage: 44.4 MB, less than 39.26% of Java online submissions for Valid Palindrome.
     public static void main(String[] args) {
         String s = "A man, a plan, a canal: Panama";
-        isPalindrome(s);
+        System.out.println(isPalindrome(s));
     }
 
     public static boolean isPalindrome(String s) {
 
-        ArrayList<String> sortedString = new ArrayList<>();
+        StringBuilder str = new StringBuilder();
+        String strNormal = "";
+        String strReverse = "";
 
         //remove all special characters
         for (int i = 0; i < s.length(); i++) {
             char currentLetter = Character.toLowerCase(s.charAt(i));
-            System.out.println(currentLetter);
-            int currentLetterASCII = currentLetter;
+//            System.out.println(currentLetter);
 
-            if (currentLetterASCII <= 122 && currentLetterASCII >= 97) {
-                sortedString.add(String.valueOf(currentLetter));
+            if ((int) currentLetter <= 122 && (int) currentLetter >= 97) {
+                str.append(currentLetter);
+            }
+            if ((int) currentLetter <= 57 && (int) currentLetter >= 48) {
+                str.append(currentLetter);
             }
         }
-        System.out.println(sortedString);
-        return true;
+        strNormal = String.valueOf(str);
+        strReverse = String.valueOf(str.reverse());
+
+        return strNormal.equals(strReverse);
     }
 }
