40bc9520a92d0efee0ad923ecc2f2e3d1b34bb36
withers56 <william.k.withers@gmail.com>
2022-06-22 10:48:07 -0500

    removed non alphanumeric values from strign and sotred in array

--- src/leetcodes/ValidPalindrome.java
+++ src/leetcodes/ValidPalindrome.java
@@ -1,14 +1,31 @@
 package leetcodes;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class ValidPalindrome {
 //    A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all
 //    non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
     public static void main(String[] args) {
         String s = "A man, a plan, a canal: Panama";
+        isPalindrome(s);
     }
 
     public static boolean isPalindrome(String s) {
 
+        ArrayList<String> sortedString = new ArrayList<>();
+
+        //remove all special characters
+        for (int i = 0; i < s.length(); i++) {
+            char currentLetter = Character.toLowerCase(s.charAt(i));
+            System.out.println(currentLetter);
+            int currentLetterASCII = currentLetter;
+
+            if (currentLetterASCII <= 122 && currentLetterASCII >= 97) {
+                sortedString.add(String.valueOf(currentLetter));
+            }
+        }
+        System.out.println(sortedString);
         return true;
     }
 }
