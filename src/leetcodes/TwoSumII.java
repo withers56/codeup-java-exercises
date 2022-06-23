1ffc214f2091debd3047f9d191a99b6217403406
withers56 <william.k.withers@gmail.com>
2022-06-23 10:38:24 -0500

    started twoSumII exercises

---
+++ src/leetcodes/TwoSumII.java
@@ -0,0 +1,22 @@
+package leetcodes;
+
+public class TwoSumII {
+//    Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order, find two numbers
+//    such that they add up to a specific target number. Let these two numbers be numbers[index1] and numbers[index2]
+//    where 1 <= index1 < index2 <= numbers.length.
+//
+//    Return the indices of the two numbers, index1 and index2, added by one as an integer array [index1, index2] of length 2.
+//
+//    The tests are generated such that there is exactly one solution. You may not use the same element twice.
+//
+//    Your solution must use only constant extra space.
+    public static void main(String[] args) {
+
+        int[] numbers = {2,7,11,15};
+        twoSum(numbers, 9);
+    }
+
+    public static int[] twoSum(int[] numbers, int target) {
+
+    }
+}
